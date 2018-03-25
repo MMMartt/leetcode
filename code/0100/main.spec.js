6ce3ad21f1a67bdf7db66cfb224ab0dabd0ff2cd
MMMartt <nielumartin@hotmail.com>
2018-03-25 17:18:43 +0800

    0100

---
+++ code/0100/main.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const solution = require('./main')
+
+describe('0100-solution', () => {
+  const makeTree = ([first, left, right]) => {
+    return {
+      val: first,
+      left: left && left.length ? makeTree(left) : null,
+      right: right && right.length ? makeTree(right) : null,
+    }
+  }
+  it('should get [1,2,null]', () => {
+    assert.equal(solution(makeTree([1, [2, null, null], null]), makeTree([1, [2, null, null], null])), true)
+  })
+  it('should get [1,2,null] [1,null,2]', () => {
+    assert.equal(solution(makeTree([1, null, [2, null, null]]), makeTree([1, [2, null, null], null])), false)
+  })
+})

e477b9deabae5c8121f9c87e4d6249bc2ba99f9f
MMMartt <nielumartin@hotmail.com>
2018-03-25 16:47:14 +0800

    0659

---
+++ code/0659/main.js
@@ -0,0 +1,87 @@
+/*You are given an integer array sorted in ascending order (may contain duplicates), you need to split them into several subsequences, where each subsequences consist of at least 3 consecutive integers. Return whether you can make such a split.
+
+  Example 1:
+Input: [1,2,3,3,4,5]
+Output: True
+Explanation:
+  You can split them into two consecutive subsequences :
+  1, 2, 3
+3, 4, 5
+Example 2:
+Input: [1,2,3,3,4,4,5,5]
+Output: True
+Explanation:
+  You can split them into two consecutive subsequences :
+  1, 2, 3, 4, 5
+3, 4, 5
+Example 3:
+Input: [1,2,3,4,4,5]
+Output: False
+Note:
+  The length of the input is in range of [1, 10000]*/
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isPossible = nums => {
+  let startIndexes = []
+  let currentPos = -1
+  let currentValue = nums[0]
+  let lastCount = 0
+  let valueCount = 1
+  if (nums.length < 3) {
+    return false
+  }
+  const seqChange = count => {
+    currentPos++
+    if (count === 0) {
+      return true
+    } else if (count > 0) {
+      for (let i = 0; i < count; i++) {
+        startIndexes.push(currentPos)
+      }
+    } else {
+      if (count + startIndexes.length < 0) {
+        return false
+      }
+      for (let i = 0; i < -count; i++) {
+        if (currentPos - startIndexes.shift() < 3) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+  let isTrue = true
+  for (let i = 1; i < nums.length; i++) {
+    const current = nums[i]
+    if (current !== currentValue) {
+      if (current !== currentValue + 1) {
+        isTrue = seqChange(valueCount-lastCount)
+        console.log(startIndexes, '0', current, valueCount, lastCount, -valueCount, isTrue, currentPos)
+        isTrue = seqChange(-valueCount)
+        console.log(startIndexes, '0', current, valueCount, lastCount, -valueCount, isTrue, currentPos)
+        lastCount = 0
+        valueCount = 1
+      } else {
+        isTrue = seqChange(valueCount - lastCount)
+        console.log(startIndexes, '1', current, valueCount, lastCount, valueCount - lastCount, isTrue, currentPos)
+        lastCount = valueCount
+        valueCount = 1
+      }
+      currentValue = current
+      if (!isTrue) {
+        return false
+      }
+    } else {
+      valueCount++
+    }
+  }
+  isTrue = seqChange(valueCount - lastCount)
+  console.log(startIndexes, '2', nums[nums.length - 1], valueCount, lastCount, valueCount - lastCount, isTrue, currentPos)
+  isTrue = seqChange(-valueCount)
+  console.log(startIndexes, '2', nums[nums.length - 1], valueCount, lastCount, -valueCount, isTrue, currentPos)
+  return isTrue
+}
+
+module.exports = isPossible

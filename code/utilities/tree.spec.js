90e2ec8f11775dd9cb92cdd7c97faf3c3084396b
MMMartt <nielumartin@hotmail.com>
2018-03-30 14:34:55 +0800

    test single node

--- code/utilities/tree.spec.js
+++ code/utilities/tree.spec.js
@@ -8,4 +8,7 @@ describe('tree utility test', () => {
   it('should get []', () => {
     assert.deepEqual([], leetcodeBfs(leetcodeArray2Node([])))
   })
+  it('should get [1]', () => {
+    assert.deepEqual([1], leetcodeBfs(leetcodeArray2Node([1])))
+  })
 })

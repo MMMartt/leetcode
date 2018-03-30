59f1aabe65511bfcc3c0570ebae777d4c2f4fff8
MMMartt <nielumartin@hotmail.com>
2018-03-30 15:08:12 +0800

    0538

---
+++ code/0538/main.js
@@ -0,0 +1,52 @@
+/*Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+  Example:
+
+Input: The root of a Binary Search Tree like this:
+     5
+  /   \
+2     13
+
+Output: The root of a Greater Tree like this:
+18
+/   \
+          20     13*/
+/**
+ * @desc: need to be removed on leetcode
+ * @param val
+ * @constructor
+ */
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const convertBST = root => {
+  const nodeStack = []
+  let node = root
+  let preSum = 0
+  while (node !== null || nodeStack.length !== 0) {
+    if (node === null) {
+      node = nodeStack.pop()
+      node.val += preSum
+      preSum = node.val
+      node = node.left
+    } else {
+      nodeStack.push(node)
+      node = node.right
+    }
+  }
+  return root
+}
+
+module.exports = convertBST
---
+++ code/0538/main.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const solution = require('./main')
+const {leetcodeBfs, leetcodeArray2Node} = require('../utilities/tree')
+
+describe('0538-solution', () => {
+  it('should get [2]', () => {
+    assert.deepEqual(
+      leetcodeBfs(solution(leetcodeArray2Node([2]))),
+      [2]
+    )
+  })
+  it('should get [5, 2, 13]', () => {
+    assert.deepEqual(
+      leetcodeBfs(solution(leetcodeArray2Node([5, 2, 13]))),
+      [18, 20, 13]
+    )
+  })
+})

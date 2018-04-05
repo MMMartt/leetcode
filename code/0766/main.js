f15cb1b95e741cf4a39cf5138e909b0d6332d580
MMMartt <nielumartin@hotmail.com>
2018-04-05 20:24:49 +0800

    0766

---
+++ code/0766/main.js
@@ -0,0 +1,52 @@
+/*A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+
+Example 1:
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: True
+Explanation:
+1234
+5123
+9512
+
+In the above grid, the diagonals are "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]", and in each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+Input: matrix = [[1,2],[2,2]]
+Output: False
+Explanation:
+The diagonal "[1, 2]" has different elements.
+Note:
+
+matrix will be a 2D array of integers.
+matrix will have a number of rows and columns in range [1, 20].
+matrix[i][j] will be integers in range [0, 99].*/
+
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+const isToeplitzMatrix = matrix => {
+  // matrix will have a number of rows and columns in range [1, 20].
+  const line = matrix.length
+  const column = matrix[0].length
+  let values = matrix[matrix.length - 1]
+  for (let i = line - 2; i > -1; i--) {
+    const curr = matrix[i]
+    // left the last element
+    let j = 0
+    for (; j < column - 1; j++) {
+      if (curr[j] !== values[line - i + j - 1]) {
+        console.log(values, curr, line, i, j)
+        return false
+      }
+    }
+    values.push(curr[j])
+  }
+  return true
+}
+
+module.exports = isToeplitzMatrix
